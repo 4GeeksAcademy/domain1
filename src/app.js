ab3bbd175429efe97416c8c0046a60bbb61be981
Arturo <141342995+amancilla88@users.noreply.github.com>
2023-10-05 00:32:43 +0000

    domain

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let multiplicationTable = [];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+for (let i = 1; i < 11; i++) {
+  for (let j = 1; j < 11; j++) {
+    multiplicationTable.push(`${i} * ${j} = ${i * j}`);
+  }
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+console.log(multiplicationTable);
+
+let prefix = ["the", "boys", "girls", "they", "kids"];
+let word = ["code", "tech", "stack", "geeks", "computing"];
+let verb = ["worldwide", "global", "universal", "everywhere", "globalized"];
+
+for (let i = 0; i < prefix.length; i++) {
+  for (let j = 0; j < word.length; j++) {
+    let div = document.createElement("div");
+    div.innerText = `${prefix[i]}${word[j]}.com`;
+    document.body.appendChild(div);
+  }
+}
